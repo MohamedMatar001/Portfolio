d43f8febb142e3cb4b4d7c10f927199d9af6f271
Mohamed <mohamed.m.matar001@gmail.com>
2025-10-16 22:07:12 +0300

    Update navItems in Navbar to correct Home link href and reorder Skills link

--- client/src/components/layouts/NavBar.jsx
+++ client/src/components/layouts/NavBar.jsx
@@ -4,9 +4,9 @@ import { Menu, X } from "lucide-react";
 
 
 const navItems = [
-  { name: "Home", href: "#home" },
-  { name: "Skills", href: "#skills" },
+  { name: "Home", href: "#hero" },
   { name: "About", href: "#about" },
+  { name: "Skills", href: "#skills" },
   { name: "Projects", href: "#projects" },
   { name: "Contact", href: "#contact" },
 ];
